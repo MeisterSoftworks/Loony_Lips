890d714df5b0cb9a71b59b91a6c2de31fa5488d4
1ntrepid MAXIMUM+ <blackmathsavior@gmail.com>
2019-02-20 20:41:07 -0800

    [2-11] LineEdit - Text From The Player

--- Loony Lips/LoonyLips.gd
+++ Loony Lips/LoonyLips.gd
@@ -4,4 +4,14 @@ func _ready():
 	var prompt = ["Jim", "UP", "excited", "Diner", "Susan"]
 	var story = "Once upon a time a %s went to see %s and started feeling very %s. Afterwards they went to a %s to meet their friend %s."
 	#get_node("Blackboard/StoryText").text = story % prompt
-	$Blackboard/StoryText.bbcode_text = story % prompt
+	$Blackboard/StoryText.text = story % prompt
+	$Blackboard/TextBox.text = ""
+
+func _on_TextureButton_pressed():
+	var new_text = get_node("Blackboard/TextBox").text
+	_on_TextBox_text_entered(new_text)
+	
+	
+func _on_TextBox_text_entered(new_text):
+	$Blackboard/StoryText.text = new_text
+	$Blackboard/TextBox.text = ""
