b07597b2a2d1e27d9783c8d7e560c54a2b903dc9
1ntrepid MAXIMUM+ <blackmathsavior@gmail.com>
2019-03-09 13:34:08 -0800

    [2-15] JSON and File

--- Loony Lips/LoonyLips.gd
+++ Loony Lips/LoonyLips.gd
@@ -1,25 +1,33 @@
 extends Node2D
 
 var player_words = [] # The number of words a player chooses
-var template = [
-		{
-		"prompt" : ["a name", "a thing", "a feeling", "a place", "another name"],
-		"story" : "Once upon a time a %s went to see %s and started feeling very %s. Afterwards they went to a %s to meet their friend %s." 
-		},
-		{
-		"prompt" : ["a location", "an action", "another action", "a feeling", "a thing", "an action"],
-		"story" : "When first entering %s, it is customary to %s the host. Doing so will make them %s with %s. This completes the greeting ceremony %s, thank you for %s." 
-		}
-		]
 var current_story
+var strings # All text shown to player
 
 func _ready():
-	randomize()
-	current_story = taemplate [randi() % template.size()]
+	set_random_story()
+	strings = get_from_json("other_strings.json")
 	
-	$Blackboard/StoryText.text = ("Welcome to Loony Lips!\nCan I have " + current_story.prompt[player_words.size()] + ", please?")
+	$Blackboard/StoryText.text = strings["intro_text"]
+	prompt_player()
+	
 	$Blackboard/TextBox.clear()
 	$Blackboard/TextBox.grab_focus() # Edit suggested by Keith/Udemy
+
+func set_random_story():
+	var stories = get_from_json("stories.json")
+	
+	randomize()
+	current_story = stories [randi() % stories.size()]
+
+func get_from_json(filename):
+	var file = File.new() # The file object
+	file.open(filename, File.READ) # Assumes the file exists
+	
+	var text = file.get_as_text()
+	var data = parse_json(text)
+	file.close()
+	return data
 
 func _on_TextureButton_pressed():
 	if is_story_done():
@@ -38,7 +46,8 @@ func is_story_done():
 	return player_words.size() == current_story.prompt.size() # Passes this information to whichever function just called this
 
 func prompt_player():
-	$Blackboard/StoryText.text = ("Can I have " + current_story.prompt[player_words.size()] + ", please?")
+	var next_prompt = current_story["prompt"][player_words.size()]
+	$Blackboard/StoryText.text += (strings["prompt"] % next_prompt)
 	
 func next_action():
 	if is_story_done():
@@ -48,7 +57,7 @@ func next_action():
 
 func tell_story():
 	$Blackboard/StoryText.text = current_story.story % player_words
-	$Blackboard/TextureButton/ButtonLabel.text = "Again!"
+	$Blackboard/TextureButton/ButtonLabel.text = strings["again"]
 	end_game()
 	
 func end_game():
