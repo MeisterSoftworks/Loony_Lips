e93c62981d76b17f1ffb170f78de5f65f8405b42
1ntrepid MAXIMUM+ <blackmathsavior@gmail.com>
2019-03-09 11:57:39 -0800

    [2-14] Dictionaries - Adding a Story Template

--- Loony Lips/LoonyLips.gd
+++ Loony Lips/LoonyLips.gd
@@ -1,11 +1,23 @@
 extends Node2D
 
 var player_words = [] # The number of words a player chooses
-var prompt = ["a name", "a thing", "a feeling", "a place", "another name"]
-var story = "Once upon a time a %s went to see %s and started feeling very %s. Afterwards they went to a %s to meet their friend %s."
+var template = [
+		{
+		"prompt" : ["a name", "a thing", "a feeling", "a place", "another name"],
+		"story" : "Once upon a time a %s went to see %s and started feeling very %s. Afterwards they went to a %s to meet their friend %s." 
+		},
+		{
+		"prompt" : ["a location", "an action", "another action", "a feeling", "a thing", "an action"],
+		"story" : "When first entering %s, it is customary to %s the host. Doing so will make them %s with %s. This completes the greeting ceremony %s, thank you for %s." 
+		}
+		]
+var current_story
 
 func _ready():
-	$Blackboard/StoryText.text = ("Welcome to Loony Lips!\nCan I have " + prompt[player_words.size()] + ", please?")
+	randomize()
+	current_story = taemplate [randi() % template.size()]
+	
+	$Blackboard/StoryText.text = ("Welcome to Loony Lips!\nCan I have " + current_story.prompt[player_words.size()] + ", please?")
 	$Blackboard/TextBox.clear()
 	$Blackboard/TextBox.grab_focus() # Edit suggested by Keith/Udemy
 
@@ -23,10 +35,10 @@ func _on_TextBox_text_entered(new_text):
 	next_action()
 
 func is_story_done():
-	return player_words.size() == prompt.size() # Passes this information to whatever function called this
+	return player_words.size() == current_story.prompt.size() # Passes this information to whichever function just called this
 
 func prompt_player():
-	$Blackboard/StoryText.text = ("Can I have " + prompt[player_words.size()] + ", please?")
+	$Blackboard/StoryText.text = ("Can I have " + current_story.prompt[player_words.size()] + ", please?")
 	
 func next_action():
 	if is_story_done():
@@ -35,7 +47,7 @@ func next_action():
 		prompt_player()
 
 func tell_story():
-	$Blackboard/StoryText.text = story % player_words
+	$Blackboard/StoryText.text = current_story.story % player_words
 	$Blackboard/TextureButton/ButtonLabel.text = "Again!"
 	end_game()
 	
